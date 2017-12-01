7ebae4a506fd4efb146faed19c7ad8df9ce9eb92
Christopher Biessener <cpbiessener@gmail.com>
2017-12-01 10:53:01 -0600

    add google fonts; throw away duplicate hits

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -15,7 +15,8 @@ var Hangman =
   cur_correct: 0,
   cur_misses:  0,
   game_over:   6,
-  a_guesses:   [],
+  a_hits:      [],
+  a_misses:    [],
   start_game: function()
   {
     this.started = true;
@@ -33,7 +34,8 @@ var Hangman =
     this.cur_misses = 0;
     this.set_gallows();
     this.d_gallows.innerHTML = this.cur_gallows;
-    this.a_guesses = [];
+    this.a_hits = [];
+    this.a_misses = [];
     this.d_guesses.textContent = "";
   },
   end_game: function(msg)
@@ -77,8 +79,8 @@ document.onkeyup = function(event)
 {
   var i, idx;
   var e_key = String.fromCharCode(event.keyCode).toLowerCase();
-  var guess_str; // string for the guesses
-  var display; // string for the display
+  var scratch;
+  var scratch; // string for the scratch
 
   // debug the event
   console.log("event:", event);
@@ -96,13 +98,19 @@ document.onkeyup = function(event)
     return false;
   }
 
-  // debug display the guess
+  // debug the key press
   console.log("e_key", e_key);
 
   // check guess vs word
   if (Hangman.check(e_key))
   {
-    // if correct, display the guess in the word
+    // if a new hit, display the guess in the word
+    scratch = Hangman.a_hits.toString();
+    if (scratch.includes(e_key))
+      return false; // not a new hit
+
+    Hangman.a_hits.push(e_key);
+    // process the hit
     for (i = 0; i < Hangman.cur_word.length; ++i)
     {
       idx = ((i+1)*2)-2;
@@ -113,37 +121,37 @@ document.onkeyup = function(event)
         console.log("replacing position " + i, "idx", idx);
         // Hangman.cur_display[((i+1)*2)-2] = e_key; // This would work in a real language!!!  Instead I have to add all sorts of extra logic to rebuild the string one piece at a time!!!
         if (i === 0)
-          display = e_key;
+          scratch = e_key;
         else
-          display += " " + e_key;
+          scratch += " " + e_key;
       } else {
         if (i === 0)
-          display = Hangman.cur_display.charAt(0);
+          scratch = Hangman.cur_display.charAt(0);
         else
-          display += " " + Hangman.cur_display.charAt(idx);
+          scratch += " " + Hangman.cur_display.charAt(idx);
       }
     }
-    Hangman.cur_display = display;
+    Hangman.cur_display = scratch;
     Hangman.d_word.textContent = Hangman.cur_display;
     console.log("cur_correct", Hangman.cur_correct);
   } else {
     // if a new miss, add the guess and increment the gallows
-    guess_str = Hangman.a_guesses.toString();
-    if (guess_str.includes(e_key.toUpperCase()))
+    scratch = Hangman.a_misses.toString();
+    if (scratch.includes(e_key.toUpperCase()))
       return false; // not a new miss
 
-    Hangman.a_guesses.push(e_key.toUpperCase());
-    // build the guess_str
-    for (i = 0; i < Hangman.a_guesses.length; ++i)
+    Hangman.a_misses.push(e_key.toUpperCase());
+    // build the string of misses
+    for (i = 0; i < Hangman.a_misses.length; ++i)
     {
       if (i === 0)
-        guess_str = Hangman.a_guesses[0];
+        scratch = Hangman.a_misses[0];
       else
-        guess_str = guess_str + " " + Hangman.a_guesses[i];
+        scratch = scratch + " " + Hangman.a_misses[i];
     }
-    // display the guesses
-    Hangman.d_guesses.textContent = guess_str;
-    // display the new gallows
+    // scratch the guesses
+    Hangman.d_guesses.textContent = scratch;
+    // scratch the new gallows
     Hangman.miss();
   }
 
